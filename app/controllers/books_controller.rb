2094b2d92f4f5b732f628350c166b9e21beccc00
jeffrothwell <rothwelljeff@gmail.com>
2018-02-04 09:16:20 -0500

    add responses for text, csv and json

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,21 @@
 class BooksController < ApplicationController
 
+  def index
+    @books = Book.all
+
+    respond_to do |format|
+      format.html
+      format.csv do
+        render plain: Book.generate_csv(@books)
+      end
+      format.json { render json: @books }
+      format.text
+    end
+
+    # respond_to do |format|
+    #   format.html
+    #   format.xml { render :xml => @people.to_xml }
+    # end
+  end
 
 end
